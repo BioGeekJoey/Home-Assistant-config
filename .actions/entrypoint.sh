6168ff2e190274dcba4d833768753be68e63f44b
Joël van der Loo <joeyjoel95@gmail.com>
2019-09-08 00:13:08 +0200

    entrypoint update

--- .actions/entrypoint.sh
+++ .actions/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-sh "echo Installing Home Assistant"
+echo "Installing Home Assistant"
 python -m pip install homeassistant
 
-sh "echo Validating Home Assistant config"
+echo "Validating Home Assistant config"
 hass -c . --script check_config --info all
