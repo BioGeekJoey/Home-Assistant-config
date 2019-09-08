fbda74a831823aa1ee850e0ae546571732e4da3c
Joël van der Loo <joeyjoel95@gmail.com>
2019-09-08 12:53:43 +0200

    testing command needed

--- .actions/entrypoint.sh
+++ .actions/entrypoint.sh
@@ -2,6 +2,9 @@
 
 echo "Installing Home Assistant"
 python3 -m pip install homeassistant
+python -m pip install homeassistant
+pip3 install homeassistant
+pip install homeassistant
 
 echo "Validating Home Assistant config"
 hass -c . --script check_config --info all
