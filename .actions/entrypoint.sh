33960d844a974ebe88053d8c98a6981fbfb5d6a8
Joël van der Loo <joeyjoel95@gmail.com>
2019-09-08 11:24:02 +0200

    Minor travis and GHA tweaks

--- .actions/entrypoint.sh
+++ .actions/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Installing Home Assistant"
-python -m pip install homeassistant
+python3 -m pip install homeassistant
 
 echo "Validating Home Assistant config"
 hass -c . --script check_config --info all
